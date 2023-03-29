631130d69454eba9ec07a7d89f97e4a893b8a404
aabidsofi19 <mailtoaabid01@gmail.com>
2023-03-29 21:22:30 +0530

    refactored code

--- src/greedy/prims.py
+++ src/greedy/prims.py
@@ -1,50 +1,71 @@
-from graph import WeightedGraph,load_weighted_edges
-from dataclasses import dataclass
+from graph import WeightedGraph, load_weighted_edges
 
 
-def prims_mst(graph:WeightedGraph) -> list:
+def prims_mst(graph: WeightedGraph) -> WeightedGraph:
+    """
+    This function finds the minimum spanning tree of a weighted graph using
+    Prim's algorithm.
 
-    """ fast implementation of prims MST algorithm using Heap """
+    :param graph: WeightedGraph object representing the graph.
+    :return: WeightedGraph object representing the minimum spanning tree of
+    the graph.
+    """
+    # Initialize the algorithm with a source vertex
+    source_vertex = 1
+    # Keep track of the vertices that have already been spanned
+    spanned_vertices = {source_vertex}
+    # Get all vertices in the graph
+    all_vertices = set(graph.vertices)
+    # Create a new graph to represent the minimum spanning tree
+    minimum_spanning_tree = WeightedGraph(graph.v)
 
-    s = 1 # source vertex to intialize the algorithm with 
+    # Continue until all vertices have been spanned
+    while spanned_vertices != all_vertices:
+        # Get all edges that cross between the spanned vertices and the
+        # unspanned vertices
+        crossing_edges = [
+            (u, v)
+            for u in spanned_vertices
+            for v in graph.adj[u]
+            if v not in spanned_vertices
+        ]
+        # Find the edge with the smallest weight
+        u, v = min(crossing_edges, key=graph.w.get)
+        # Add the edge and its weight to the minimum spanning tree
+        minimum_spanning_tree.add_edge(u, v, graph.w[(u, v)])
+        # Add the newly spanned vertex to the set of spanned vertices
+        spanned_vertices.add(v)
 
-    spanned = {s}
-    V = set(graph.vertices)
-    T = WeightedGraph(graph.v) 
-
-    while spanned != V :
-
-       crossing_edges = [ (u,v) for u in spanned for v in graph.adj[u] if v not in spanned ]
-    #    print("crossing",crossing_edges)
-       u,v = min(crossing_edges, key= graph.w.get )
-    #    print("min",(u,v)) 
-       T.add_edge(u,v , graph.w[(u,v)]) 
-       spanned.add(v)
-
-    return T 
-
-       
+    # Return the minimum spanning tree
+    return minimum_spanning_tree
 
 
-def test(file,correct_cost)  :
-    print(f"[+] testing {file}")
-    graph = load_weighted_edges(file)
-   #  print("graph",graph.adj,graph.w)
+def test(file_path: str, correct_cost: float) -> None:
+    """
+    This function tests the `prims_mst` function on a given graph file and
+    expected minimum spanning tree cost.
+
+    :param file_path: A string representing the path to the graph file.
+    :param correct_cost: A float representing the expected minimum spanning
+    tree cost.
+    :return: None
+    """
+    # Print the name of the file being tested
+    print(f"[+] Testing {file_path}")
+    # Load the graph from the file
+    graph = load_weighted_edges(file_path)
+    # Find the minimum spanning tree of the graph
     mst = prims_mst(graph)
-    cost =   sum(mst.w.values())
-   #  print("mst" , mst.w) 
-    print("cost of mst" , cost )
+    # Calculate the cost of the minimum spanning tree
+    cost = sum(mst.w.values())
+    # Print the cost of the minimum spanning tree
+    print("Cost of minimum spanning tree:", cost)
+    # Check that the calculated cost matches the expected cost
     assert cost == correct_cost
 
 
-     
-test("small_graph.testcase",7)
-test("prim_2.testcase",3)
-test("prims.testcase",20934)
+if __name__ == "__main__":
 
-
-
-
-
-
-
+    test("small_graph.testcase", 7)
+    test("prim_2.testcase", 3)
+    test("prims.testcase", 20934)
--- src/greedy/scheduling.py
+++ src/greedy/scheduling.py
@@ -1,57 +1,91 @@
-from typing import Tuple,List ,Callable 
-from functools import reduce 
+from typing import Tuple, List, Callable
 
-JOBS = List[Tuple[int,int]] # LIST[ ( weight , length )]
+# Define type alias for jobs list
+JOBS = List[Tuple[int, int]]  # LIST[ (weight, length) ]
 
 
-def calc_weigthed_sum(jobs : JOBS ) :
+def calc_weighted_sum(jobs: JOBS) -> int:
 
-    completion_time = 0 
-    sum_ = 0 
+    """
+    Calculates the weighted sum of completion times of the given jobs.
 
-    for job in jobs :
-        weight , length = job 
-        completion_time += length 
+    :param jobs: A list of job tuples, where each tuple contains two integers:
+    the job weight and length.
+    :return: An integer representing the weighted sum of completion times.
+    """
+    completion_time = 0
+    sum_ = 0
+
+    # Iterate through jobs and calculate completion times and sum
+    for job in jobs:
+        weight, length = job
+        completion_time += length
         sum_ += completion_time * weight
 
-    return sum_  
+    return sum_
 
 
+def schedule_jobs(
+    jobs: JOBS, greedy_criteria: Callable[[int, int], int | float]
+) -> int:
+    """
+    Schedules the given jobs according to the specified greedy criteria and
+    returns the weighted sum of completion times.
 
-def schedule_jobs(jobs : JOBS , greedy_criteria : Callable[[int,int],int|float] ) -> int :
-    """ shedules the jobs and returns the weighted sum of completion times """
+    :param jobs: A list of job tuples, where each tuple contains two integers:
+    the job weight and length.
+    :param greedy_criteria: A function that takes two integers representing
+    the job weight and length, and returns a score
+                            that is used to sort the jobs in decreasing order.
+    :return: An integer representing the weighted sum of completion times of
+    the scheduled jobs.
+    """
+    # Sort the jobs using the specified greedy criteria
+    scheduled = sorted(
+        jobs, key=lambda job: greedy_criteria(job[0], job[1]), reverse=True
+    )
+    # Calculate the weighted sum of completion times of the scheduled jobs
+    return calc_weighted_sum(scheduled)
 
-    scheduled = sorted(jobs,key = lambda job : greedy_criteria(job[0],job[1]),reverse=True) 
-    # print("scheduled_jobs" , scheduled)
-    return calc_weigthed_sum(scheduled)
 
+def load_jobs(file_path: str) -> JOBS:
+    """
+    Loads job data from a file and returns a list of job tuples.
 
-
-def load_jobs(file_name : str ) :
-    with open(file=file_name) as f :
+    :param file_path: A string representing the path to the file containing
+    the job data.
+    :return: A list of job tuples, where each tuple contains two integers: the
+    job weight and length.
+    """
+    with open(file=file_path) as f:
         lines = f.readlines()
         jobs = []
-        for job in lines[1:] : 
-            w , l = map(int ,job.split(" "))
-            jobs.append((w,l))
+        for job in lines[1:]:
+            w, l = map(int, job.split(" "))
+            jobs.append((w, l))
 
-    return jobs 
+    return jobs
 
 
+def test(file_path: str) -> None:
+    """
+    Runs tests on the scheduling algorithm using job data from a file.
 
-# jobs = [(3,1),(2,2),(1,3)]
-# jobs = [(5, 6), (4, 1), (6, 7)]
+    :param file_path: A string representing the path to the file containing
+    the job data.
+    :return: None
+    """
+    # Print the name of the file being tested
+    print(f"[+] Testing {file_path}")
+    # Load the job data from the file
+    jobs = load_jobs(file_path)
+    # Schedule the jobs using the difference greedy criterion
+    schedule_1 = schedule_jobs(jobs, greedy_criteria=lambda w, l: (w - l, w))
+    # Schedule the jobs using the ratio greedy criterion
+    schedule_2 = schedule_jobs(jobs, greedy_criteria=lambda w, l: w / l)
+    # Print the results of the two schedules
+    print("Schedule using difference criterion:", schedule_1)
+    print("Schedule using ratio criterion:", schedule_2)
 
-def test(file ) :
-
-  print(f"[+] testing {file}")
-  jobs = load_jobs(file)
-
-  schedule_1 = schedule_jobs(jobs, greedy_criteria = lambda w , l : ( w - l  , w )) 
-  schedule_2 = schedule_jobs(jobs, greedy_criteria = lambda w , l : w / l  ) 
-
-  print(f"schedule using difference",schedule_1)
-  print(f"schedule using ratio",schedule_2) 
-  assert schedule_2 <= schedule_1
-
-test("jobs.testcase")
+    # because schedule_2 is most optimal
+    assert schedule_2 <= schedule_1
